302d23dc66107fa68865d506ce56e139d3a113f7
NirmalQA <nirmalthanga@gmail.com>
2018-02-05 18:56:24 -0500

    New changes

--- src/main/java/pack_Reports/ExtentReportNG.java
+++ src/main/java/pack_Reports/ExtentReportNG.java
@@ -1,18 +1,17 @@
 package pack_Reports;
 
 
-import java.io.File;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-
-
 import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 import org.testng.*;
 import org.testng.xml.XmlSuite;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class ExtentReportNG implements IReporter {
     private ExtentReports extent;
